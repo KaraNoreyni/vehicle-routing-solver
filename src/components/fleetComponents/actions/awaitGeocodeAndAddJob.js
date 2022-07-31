3113351f708c08e7d6503c92bf4125ef519755b4
nonoXR <69noreyni.sery@gmail.com>
2022-07-31 14:35:32 +0200

    awaitGeocodAndAddJob

---
+++ src/components/fleetComponents/actions/awaitGeocodeAndAddJob.js
@@ -0,0 +1,35 @@
+import { geocode } from "../../../actions/getGeocode";
+import { addJob } from "../../../reducer/jobsReducer";
+import { Store } from "../../../reducer";
+
+export const awaitGeocodeAndAddJob = async function (text, date, index) {
+    console.log(text)
+    const dispatch = Store.dispatch
+    console.log('waiting for geocode');
+
+    try {
+      const location = await geocode(text);
+      const job = {
+          id: text,
+          deliveries: [{
+            places: [{
+                location: {
+                    index: index,
+                    lat: location.lat(),
+                    lng: location.lng(),
+                },
+                duration: 300,
+                times: [date]
+              }],
+            demand: [
+                1
+            ],
+            }]
+      };
+      dispatch(addJob(job));
+    } catch (error) {
+        console.log(error)
+        // setState(error)
+    }
+
+  }
---
+++ src/components/fleetComponents/actions/awaitGeocodeAndEditJob.js
@@ -0,0 +1,21 @@
+import { geocode } from "../../../actions/getGeocode";
+import { editJob } from "../../../reducer/jobsReducer";
+import { Store } from "../../../reducer";
+
+export const awaitGeocodeAndEditJob = async function (payload) {
+    const dispatch = Store.dispatch
+    console.log('waiting for geocode');
+    try {
+        const location = await geocode(payload.address);
+        payload.location = {
+            index: payload.jobIndex,
+            lat: location.lat(),
+            lng: location.lng()
+        }
+      dispatch(editJob(payload));
+    } catch (error) {
+        console.log(error)
+        // setState(error)
+    }
+
+}
